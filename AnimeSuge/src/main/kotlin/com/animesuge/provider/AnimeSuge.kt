3f1169bf68c3e7245e4ef4c3aebf3224fe5440d2
kim20598 <ki688494@gmail.com>
2025-12-16 15:23:30 +0100

    Update AnimeSuge.kt

--- AnimeSuge/src/main/kotlin/com/animesuge/provider/AnimeSuge.kt
+++ AnimeSuge/src/main/kotlin/com/animesuge/provider/AnimeSuge.kt
@@ -11,7 +11,7 @@ import com.lagradost.cloudstream3.utils.newExtractorLink
 import com.lagradost.cloudstream3.utils.getQualityFromName
 import org.jsoup.nodes.Element
 
-class AnimeSuge : MainAPI() {
+class AnimeSugeProvider : MainAPI() {
     override var mainUrl = "https://animesuge.bz"
     override var name = "Animesuge"
     override val hasMainPage = true
