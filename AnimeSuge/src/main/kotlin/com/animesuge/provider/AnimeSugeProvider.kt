0c394170163cece56d122760d37f2c9d1c410c62
kim20598 <ki688494@gmail.com>
2025-12-17 16:33:33 +0100

    Update AnimeSugeProvider.kt

--- AnimeSuge/src/main/kotlin/com/animesuge/provider/AnimeSugeProvider.kt
+++ AnimeSuge/src/main/kotlin/com/animesuge/provider/AnimeSugeProvider.kt
@@ -1,12 +1,15 @@
 package com.animesuge.provider
 
-import android.content.Context
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class AnimeSugeProvider : Plugin() {
-    override fun load(context: Context) {
+class AnimeSugePlugin : BasePlugin() {
+    override fun load() {
         registerMainAPI(AnimeSuge())
+        // Register extractors
+        registerExtractorAPI(AnimeSugeMegaPlay())
+        registerExtractorAPI(AnimeSugeStreamWish())
+        registerExtractorAPI(AnimeSugeFileMoon())
     }
 }
