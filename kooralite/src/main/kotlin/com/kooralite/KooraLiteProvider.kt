277468db40f0360e982d2de361eb64f2f04e0e92
kim20598 <ki688494@gmail.com>
2025-12-17 21:13:18 +0100

    Add files via upload

---
+++ kooralite/src/main/kotlin/com/kooralite/KooraLiteProvider.kt
@@ -0,0 +1,14 @@
+package com.kooralite
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class KooraLiteProvider : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(KooraLite())
+    }
+
+}
+
