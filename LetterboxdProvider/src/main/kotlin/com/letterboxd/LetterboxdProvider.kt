2d783e5bc5f7faf779803e9c574cc9867270ff81
kim20598 <ki688494@gmail.com>
2025-12-19 18:01:27 +0100

    Update LetterboxdProvider.kt

--- LetterboxdProvider/src/main/kotlin/com/letterboxd/LetterboxdProvider.kt
+++ LetterboxdProvider/src/main/kotlin/com/letterboxd/LetterboxdProvider.kt
@@ -3,23 +3,10 @@ package com.letterboxd
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.letterboxd.utils.ProviderRegistry
-import com.letterboxd.utils.SettingsManager
 
 @CloudstreamPlugin
 class LetterboxdProvider : Plugin() {
     override fun load(context: Context) {
-        // Initialize settings and provider registry
-        SettingsManager.initialize(context)
-        ProviderRegistry.initialize(context)
-        
-        // Register the main provider
         registerMainAPI(Letterboxd())
-        
-        // Optionally register settings
-        openSettings = {
-            // You could open a settings activity here
-            // For now, we'll rely on Cloudstream's built-in settings
-        }
     }
 }
