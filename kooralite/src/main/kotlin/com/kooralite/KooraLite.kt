de089876ca6fbbb23e40f9abb2847c2e00074aa3
kim20598 <ki688494@gmail.com>
2025-12-19 20:06:07 +0100

    Update KooraLite.kt

--- kooralite/src/main/kotlin/com/kooralite/KooraLite.kt
+++ kooralite/src/main/kotlin/com/kooralite/KooraLite.kt
@@ -6,7 +6,7 @@ import org.jsoup.nodes.Element
 import java.net.URLEncoder
 
 class KooraLite : MainAPI() {
-    override var mainUrl = "https://www.fullmatch-hd.com"
+    override var mainUrl = "https://www.bein-live.com"
     override var name = "KooraLite - كورة لايت"
     override var lang = "ar"
     override val hasMainPage = true
