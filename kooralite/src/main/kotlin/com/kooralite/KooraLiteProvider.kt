7f0eb7c59542558f8b21a6682bdbb3b564e8fc7b
kim20598 <ki688494@gmail.com>
2025-12-24 18:55:33 +0100

    Update KooraLiteProvider.kt

--- kooralite/src/main/kotlin/com/kooralite/KooraLiteProvider.kt
+++ kooralite/src/main/kotlin/com/kooralite/KooraLiteProvider.kt
@@ -7,8 +7,8 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class KooraLiteProvider : Plugin() {
     override fun load(context: Context) {
+        // Register both providers
         registerMainAPI(KooraLite())
+        registerMainAPI(BeinLive())
     }
-
 }
-
