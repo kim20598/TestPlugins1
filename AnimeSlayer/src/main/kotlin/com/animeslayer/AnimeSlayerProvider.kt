a82e0efb396f98b290ab048d02931ab2c440f713
kim20598 <ki688494@gmail.com>
2026-01-24 08:59:13 -0800

    Update AnimeSlayerProvider.kt

--- AnimeSlayer/src/main/kotlin/com/animeslayer/AnimeSlayerProvider.kt
+++ AnimeSlayer/src/main/kotlin/com/animeslayer/AnimeSlayerProvider.kt
@@ -1,4 +1,4 @@
-package com.animeslayer.provider
+package com.animeslayer
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
@@ -6,13 +6,6 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class AnimeSlayerProvider : Plugin() {
     override fun load() {
-        // Register main AnimeSlayer provider
         registerMainAPI(AnimeSlayer())
-        
-        // Register extractor for video sources
-        registerExtractorAPI(AnimeSlayerExtractor())
-        
-        // Note: Additional extractors can be registered here for specific video hosts
-        // that Anime Slayer uses (like Streamtape, Voe, etc.)
     }
 }
