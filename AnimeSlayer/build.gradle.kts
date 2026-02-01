fbd6d37b2bfffbfae7c74ffd2d40827313911ac9
kim20598 <ki688494@gmail.com>
2026-02-01 13:56:56 -0800

    Update build.gradle.kts

--- AnimeSlayer/build.gradle.kts
+++ AnimeSlayer/build.gradle.kts
@@ -2,8 +2,9 @@ version = 1
 
 cloudstream {
     language = "ar"
-    authors = listOf("YourName")
+    authors = listOf("kim20598")
     status = 1
     tvTypes = listOf("Anime", "AnimeMovie", "OVA")
-    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&url=https://animeslayerweb.com&size=256"
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://animeslayerweb.com&size=%size%"
 }
+
